02827a07a2dbc435f12d56997c99e47af822db68
lihoruss <ohmygadblle@gmail.com>
2025-04-10 20:18:35 +0700

    add new deploy

--- 1.sh
+++ 1.sh
@@ -4,3 +4,5 @@ sudo apt-get install -y curl gnupg lsb-release
 curl -fsSL https://openresty.org/package/pubkey.gpg | sudo tee /etc/apt/trusted.gpg.d/openresty.asc
 
 echo "deb http://openresty.org/package/debian $(lsb_release -c | awk '{print $2}') main" | sudo tee /etc/apt/sources.list.d/openresty.list
+
+sudo apt-get install nginx-module-lua
